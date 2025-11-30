917a46deb9a762e527d297917b7fc90a0547ee62
belhaj-zin <zinbelhaj58@gmail.com>
2025-11-30 15:19:36 +0100

    deleted test file

--- include/abr_management.h
+++ include/abr_management.h
@@ -5,10 +5,19 @@
 
 // Binary Search Tree function declarations
 
-// TODO: Add function declarations for:
-// - Insert user into BST
-// - Search user in BST
-// - Delete user from BST
-// - Traverse BST (in-order, pre-order, post-order)
+//! Add function declarations :
+//? - Insert user into BST :
+
+node* InsertUser_to_node(node* tree,int id ,char *name);
+void InsertUser(BST *tree, int id, char *name);
+
+//? - Search user in BST :
+
+node* searchUser_node(node *tree,int searched_id);
+node* searchUser(BST *tree, int searched_id);
+
+//? - Delete user from BST :
+
+//? - Traverse BST (in-order, pre-order, post-order) :
 
 #endif // ABR_MANAGEMENT_H
