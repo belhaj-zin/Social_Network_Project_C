fa70a2d54a0f266590af1fa8836bd58b5d047f14
mohamedaminbellaradh-stack <mohamedamin.bellaradh@ensi-uma.tn>
2025-12-06 23:56:27 +0100

    fix : fixed the remove relationship function in the main menu

--- include/affichage.h
+++ include/affichage.h
@@ -1,6 +1,6 @@
 #ifndef USER_INTERFACE_H
 #define USER_INTERFACE_H
-
+#include <relations_ll.h>
 #include "structures.h"
 
 // Helper function for menu display
@@ -10,14 +10,6 @@ void add_separator();
 void generate_timeline(int user_id);
 void display_friendships(int user_id);
 void display_subscriptions(int user_id);
-
-// Relationship removal functions (from relations_ll.c)
-void remove_friend(node *user1, node *user2);
-void remove_subscription(node *follower, node *followed_user);
-
-// UI wrappers for relationship removal
-void remove_friendship_ui(int user_id);
-void remove_subscription_ui(int user_id);
 
 // Main menu function
 void main_menu(int user_id);
