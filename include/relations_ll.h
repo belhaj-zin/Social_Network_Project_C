fe127aa99befe37dc59ea3e1490ca655b28edcef
nourine008 <nourinecherifa.bdiri@ensi-uma.tn>
2025-12-07 19:38:18 +0100

    feat: i did the header file

--- include/relations_ll.h
+++ include/relations_ll.h
@@ -2,16 +2,24 @@
 #define RELATIONS_LL_H
 
 #include "structures.h"
-
 // Linked List function declarations for relationships
 
-// TODO: Add function declarations for:
-// - Add relationship (friend, follow, etc.)
-// - Remove relationship
-// - Display relationships
-// - Search for specific relationship
+
+//*Check if a relationship exists in a liked list *//
+int estEnRelation(LLnode* liste, int ID_Cible);
+
+//*Add a relationship to a linked list (friend or follower)*//
+LLnode* ajouterRelation(LLnode* liste, int ID_Cible);
+
+//* Remove a relationship from a liked list (friend or follower)*//
+LLnode* supprimerRelation(LLnode* liste, int ID_Cible);
+
+//*Ajouter une relation d'amitié (bidirectionnelle)*//
+void ajouterAmi(BST* abr, int ID1, int ID2);
+
+//*S'abonner à un utilisateur (unidirectionnel)
+void sAbonner(BST* abr, int ID_Source, int ID_Cible);
 
 #endif // RELATIONS_LL_H
 
 
-// bonjour 
