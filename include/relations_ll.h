c5491ee2c386faa85b2f9bcba31a5c01f4d27df0
belhaj-zin <zinbelhaj58@gmail.com>
2025-12-07 16:25:12 +0100

    feat : added a function that counts the elements of a linked list

--- include/relations_ll.h
+++ include/relations_ll.h
@@ -10,5 +10,9 @@
 // - Remove relationship
 // - Display relationships
 // - Search for specific relationship
+// Function to count the number of relationships in the linked list
+int count_ll_nodes(LLnode* head);
+// a function that writes the linked list to a binary file
+void write_relations_to_file(LLnode* head, FILE* file) ;
 
 #endif // RELATIONS_LL_H
--- src/relations_ll.c
+++ src/relations_ll.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/relations_ll.h"
+#include "../include/structures.h"
+#include "../include/abr_management.h"
 
 // Linked List functions for relationships implementation
 
@@ -9,3 +11,24 @@
 // TODO: Implement Remove function
 
 // TODO: Implement Display/Traverse function
+
+// count the number of relationships in the linked list
+int count_ll_nodes(LLnode* head) {
+    int count = 0;
+    LLnode* current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+// a function that writes the linked list to a binary file
+void write_relations_to_file(LLnode* head, FILE* file) {
+    LLnode* current = head;
+    while (current != NULL) {
+        // Assuming your LLnode holds the friend's ID in current->id
+        fwrite(&(current->id), sizeof(int), 1, file);
+        current = current->next;
+    }
+}
